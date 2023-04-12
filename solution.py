64732e361b5de2ae18849271ab8f9f0daa6335b7
Eduard <eduardkarapetyan1997@mail.ru>
2023-04-12 13:10:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,12 +5,14 @@ from scipy.stats import anderson_ksamp
 
 chat_id = 298754188 # Ваш chat ID, не меняйте название переменной
 
+def solution(x: np.array, y: np.array) -> bool:
+    return stats.cramervonmises_2samp(x, y).pvalue < 0.01 # Ваш ответ, True или False
 # def solution(x: np.array, y: np.array) -> bool:
 #     # Измените код этой функции
 #     # Это будет вашим решением
 #     # Не меняйте название функции и её аргументы
     
 #     return anderson_ksamp([x, y]).pvalue < 0.01 # Ваш ответ, True или False
-def solution(x: np.array, y: np.array) -> bool:
-    res = stats.cramervonmises_2samp(x, y)
-    return res.pvalue < 0.01
+# def solution(x: np.array, y: np.array) -> bool:
+#     res = stats.cramervonmises_2samp(x, y)
+#     return res.pvalue < 0.01
