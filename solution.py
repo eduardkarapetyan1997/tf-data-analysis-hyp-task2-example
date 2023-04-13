bea427ad561f62c0ee4dac58b66b856c519a9429
Eduard <eduardkarapetyan1997@mail.ru>
2023-04-13 14:57:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-from scipy.stats import anderson_ksamp
+import scipy.stats as sps
+from hyppo.ksample import MMD
 
 chat_id = 298754188 # Ваш chat ID, не меняйте название переменной
-# 13/04/23 12:30
+# 13/04/23 14:55
 def solution(x: np.array, y: np.array) -> bool:
-    res = stats.cramervonmises_2samp(x, y)
-    return res.pvalue < 0.01
+      alpha = 0.01
+      res=MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+      return res < alpha
+
+
+# # 13/04/23 12:30
+# def solution(x: np.array, y: np.array) -> bool:
+#     res = stats.cramervonmises_2samp(x, y)
+#     return res.pvalue < 0.01
 
 
 # def solution(x: np.array, y: np.array) -> bool:
